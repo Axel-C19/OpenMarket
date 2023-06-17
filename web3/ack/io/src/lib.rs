9f5a2e0d33362b26010659510ad2b7306f067e98
MrJ8585 <josea.solis@cetys.edu.mx>
2023-06-17 04:57:54 -0700

    ayudame dios

--- web3/ack/io/src/lib.rs
+++ web3/ack/io/src/lib.rs
@@ -1,22 +1,14 @@
 #![no_std]
 
-use gear_lib::non_fungible_token::token::TokenMetadata;
-use gear_lib::non_fungible_token::{
-    io::{NFTApproval, NFTTransfer, NFTTransferPayout},
-    royalties::*,
-    state::NFTState,
-    token::*,
-};
 use gmeta::{In, InOut, Metadata};
 use gstd::{prelude::*, ActorId};
 
-pub use gear_lib::non_fungible_token::delegated::DelegatedApproveMessage;
 use primitive_types::H256;
 
 pub struct ContractMetadata;
 
 impl Metadata for ContractMetadata {
-    type Init = ();
+    type Init = In<InitContract>;
     type Handle = InOut<ContractAction, ContractEvent>;
     type Reply = ();
     type Others = ();
@@ -24,25 +16,24 @@ impl Metadata for ContractMetadata {
     type State = IoContractState;
 }
 
-#[derive(Debug, Encode, Decode, TypeInfo)]
+#[derive(Debug, Decode, Encode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
-pub enum ContractAction {
-    Mint {
-        transaction_id: u64,
-        token_metadata: ContractTokenMetaData,
+pub enum FTAction {
+    Mint(ContractTokenMetaData, objeto:ActorId),
+    Burn(u128),
+    Transfer {
+        from: ActorId,
+        to: ActorId,
+        users: ContractMetadata,
+        objeto: ActorId,
     },
-    AckAction {
-        user_wallet: ActorId,
-        role: String,
-        token_id: TokenId,
+    Approve {
+        to: ActorId,
+        amount: u128,
     },
-    Owner {
-        token_id: TokenId,
-    },
-    Clear {
-        transaction_hash: H256,
-    },
+    TotalSupply,
+    BalanceOf(ActorId),
 }
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
@@ -57,16 +48,26 @@ pub struct InitContract {
 #[scale_info(crate = gstd::scale_info)]
 pub struct ContractTokenMetaData {
     client_wallet: ActorId,
-    seller_waller: ActorId,
+    seller_wallet: ActorId,
+    objeto: ActorId,
 }
 
 #[derive(Encode, Decode, TypeInfo, Debug, Clone)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub enum ContractEvent {
-    Transfer(NFTTransfer),
-    AckAction(),
-    Owner { owner: ActorId, token_id: TokenId },
+    Transfer {
+        from: ActorId,
+        to: ActorId,
+        users: ContractMetadata,
+        objeto: ActorId,
+    },
+    Approve {
+        to: ActorId,
+        amount: u128,
+    },
+    TotalSupply,
+    BalanceOf(ActorId),
 }
 
 #[derive(Debug, Clone, Default, Encode, Decode, TypeInfo)]
@@ -85,9 +86,10 @@ pub struct IoContractState {
     pub seller: User,
     pub client: User,
     pub closed: bool,
-    pub owner_by_id: Vec<(TokenId, ActorId)>,
-    pub token_metadata_by_id: Vec<(TokenId, Option<TokenMetadata>)>,
-    pub tokens_for_owner: Vec<(ActorId, Vec<TokenId>)>,
+    pub objeto: ActorId,
+    pub balances: Vec<(ActorId, u128)>,
+    pub allowances: Vec<(ActorId, Vec<(ActorId, u128)>)>,
+    pub decimals: u8,
 }
 
 pub struct IoContract {
@@ -100,46 +102,5 @@ pub struct ContractState {
     seller: User,
     client: User,
     closed: bool,
-    owner_by_id: Vec<(TokenId, ActorId)>,
-    token_metadata_by_id: Vec<(TokenId, Option<TokenMetadata>)>,
-    tokens_for_owner: Vec<(ActorId, Vec<TokenId>)>,
-}
-
-impl From<&ContractState> for IoContractState {
-    fn from(value: &ContractState) -> Self {
-        let ContractState {
-            name,
-            seller,
-            client,
-            closed,
-            owner_by_id,
-            token_metadata_by_id,
-            tokens_for_owner,
-        } = value;
-
-        let owner_by_id = owner_by_id
-            .iter()
-            .map(|(hash, actor_id)| (*hash, *actor_id))
-            .collect();
-
-        let token_metadata_by_id = token_metadata_by_id
-            .iter()
-            .map(|(id, metadata)| (*id, metadata.clone()))
-            .collect();
-
-        let tokens_for_owner = tokens_for_owner
-            .iter()
-            .map(|(id, tokens)| (*id, tokens.clone()))
-            .collect();
-
-        Self {
-            name: name.clone(),
-            seller: seller.clone(),
-            client: client.clone(),
-            closed: *closed,
-            owner_by_id,
-            token_metadata_by_id,
-            tokens_for_owner,
-        }
-    }
+    objeto: ActorId,
 }
--- web3/ack/src/contract.rs
+++ web3/ack/src/contract.rs
@@ -9,60 +9,226 @@ use nft_io::{
 };
 use primitive_types::{H256, U256};
 
-#[derive(Debug, Default, NFTStateKeeper, NFTCore, NFTMetaState)]
-pub struct Contract {
-    pub token: ContractState,
-    pub token_id: TokenId,
-    pub owner: ActorId,
-    pub transactions: HashMap<H256, ContractEvent>,
+#[derive(Debug, Clone, Default)]
+struct FungibleToken {
+    //primer actor es el objeto, el segundo hashmap son los actores de los usaurios
+    handshake: HashMap<ActorId, HashMap<ActorId, ActorId>>;
+    name: String,
+    symbol: String,
+    total_supply: u128,
+    balances: HashMap<ActorId, u128>,
+    allowances: HashMap<ActorId, HashMap<ActorId, u128>>,
+    pub decimals: u8,
 }
 
-static mut CONTRACT: Option<Contract> = None;
+static mut FUNGIBLE_TOKEN: Option<FungibleToken> = None;
+
+impl FungibleToken {
+    /// Executed on receiving `fungible-token-messages::MintInput`.
+    fn mint(&mut self, amount: u128, users: ContractTokenMetaData, objeto:ActorId) {
+        self.handshake
+            .entry(msg::source())
+            .or_insert(amount);
+        msg::reply(
+            FTEvent::Transfer {
+                from: ZERO_ID,
+                to: msg::source(),
+                users,
+                objeto,
+            },
+            0,
+        )
+        .unwrap();
+    }
+    /// Executed on receiving `fungible-token-messages::BurnInput`.
+    fn burn(&mut self, amount: u128) {
+        if self.balances.get(&msg::source()).unwrap_or(&0){
+            panic!("Watafac haces mamawebo")
+        }
+
+        msg::reply(
+            FTEvent::Transfer {
+                from: ZERO_ID,
+                to: msg::source(),
+                users,
+                objeto,
+            },
+            0,
+        )
+        .unwrap();
+    }
+    /// Executed on receiving `fungible-token-messages::TransferInput` or `fungible-token-messages::TransferFromInput`.
+    /// Transfers `amount` tokens from `sender` account to `recipient` account.
+    fn transfer(&mut self, from: &ActorId, to: &ActorId, objeto: &ActorId) {
+        if from == &ZERO_ID || to == &ZERO_ID {
+            panic!("Zero addresses");
+        };
+        self.balances
+            .entry(*from)
+            .and_modify(|balance| *balance -= amount);
+        self.balances
+            .entry(*to)
+            .and_modify(|balance| *balance += amount)
+            .or_insert(amount);
+        msg::reply(
+            FTEvent::Transfer {
+                from: *from,
+                to: *to,
+                amount,
+            },
+            0,
+        )
+        .unwrap();
+    }
+
+    /// Executed on receiving `fungible-token-messages::ApproveInput`.
+    fn approve(&mut self, to: &ActorId, amount: u128) {
+        if to == &ZERO_ID {
+            panic!("Approve to zero address");
+        }
+        self.allowances
+            .entry(msg::source())
+            .or_default()
+            .insert(*to, amount);
+        msg::reply(
+            FTEvent::Approve {
+                from: msg::source(),
+                to: *to,
+                amount,
+            },
+            0,
+        )
+        .unwrap();
+    }
+
+    fn can_transfer(&mut self, from: &ActorId, amount: u128) -> bool {
+        if from == &msg::source()
+            || from == &exec::origin()
+            || self.balances.get(&msg::source()).unwrap_or(&0) >= &amount
+        {
+            return true;
+        }
+        if let Some(allowed_amount) = self
+            .allowances
+            .get(from)
+            .and_then(|m| m.get(&msg::source()))
+        {
+            if allowed_amount >= &amount {
+                self.allowances.entry(*from).and_modify(|m| {
+                    m.entry(msg::source()).and_modify(|a| *a -= amount);
+                });
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn common_state() -> <ContractTokenMetaData as Metadata>::State {
+    let state = static_mut_state();
+    let FungibleToken {
+        name,
+        client_wallet,
+        seller_wallet,
+        objeto,
+    } = state.clone();
+
+    IoFungibleToken {
+        name,
+        client_wallet,
+        seller_wallet,
+        objeto,
+    }
+}
+
+fn static_mut_state() -> &'static mut FungibleToken {
+    unsafe { FUNGIBLE_TOKEN.get_or_insert(Default::default()) }
+}
 
 #[no_mangle]
-unsafe extern "C" fn init() {
-    let config: InitContract = msg::load().expect("Unable to decode InitContract");
-    let contrato = Contract {
-        token: ContractState {
-            name: config.name,
-            seller: config.seller,
-            client: config.client,
-            closed: config.closed,
-            ..Default::default()
-        },
-        owner: msg::source(),
+extern "C" fn state() {
+    reply(common_state())
+        .expect("Failed to encode or reply with `<AppMetadata as Metadata>::State` from `state()`");
+}
+
+#[no_mangle]
+extern "C" fn metahash() {
+    let metahash: [u8; 32] = include!("../.metahash");
+    reply(metahash).expect("Failed to encode or reply with `[u8; 32]` from `metahash()`");
+}
+
+fn reply(payload: impl Encode) -> GstdResult<MessageId> {
+    msg::reply(payload, 0)
+}
+
+#[no_mangle]
+extern "C" fn handle() {
+    let action: FTAction = msg::load().expect("Could not load Action");
+    let ft: &mut FungibleToken = unsafe { FUNGIBLE_TOKEN.get_or_insert(Default::default()) };
+    match action {
+        FTAction::Mint(ContractTokenMetaData, objeto:ActorId) => {
+            ft.mint(ContractTokenMetaData, objeto:ActorId);
+        }
+        FTAction::Burn(amount) => {
+            ft.burn(amount);
+        }
+        FTAction::Transfer { from, to, objeto } => {
+            ft.transfer(&from, &to, &objeto);
+        }
+        FTAction::Approve { to, amount } => {
+            ft.approve(&to, amount);
+        }
+        FTAction::TotalSupply => {
+            msg::reply(FTEvent::TotalSupply(ft.total_supply), 0).unwrap();
+        }
+        FTAction::BalanceOf(account) => {
+            let balance = ft.balances.get(&account).unwrap_or(&0);
+            msg::reply(FTEvent::Balance(*balance), 0).unwrap();
+        }
+    }
+}
+
+#[no_mangle]
+extern "C" fn init() {
+    let config: InitConfig = msg::load().expect("Unable to decode InitConfig");
+    let ft = FungibleToken {
+        name: config.name,
         ..Default::default()
     };
-    CONTRACT = Some(contrato);
+    unsafe { FUNGIBLE_TOKEN = Some(ft) };
 }
 
 #[no_mangle]
-unsafe extern "C" fn handle() {
-    let action: ContractAction = msg::load().expect("Could not load ContractAction");
-    let contrato = CONTRACT.get_or_insert(Default::default());
-    match action {
-        ContractAction::Mint {
-            transaction_id,
-            token_metadata,
-        } => {
-            msg::reply(
-                contrato.process_transaction(transaction_id, |contrato| {
-                    ContractEvent::Transfer(MyNFTCore::mint(contrato, token_metadata))
-                }),
-                0,
-            )
-            .expect("Error during replying with `NFTEvent::Transfer`");
+extern "C" fn meta_state() -> *mut [i32; 2] {
+    let query: State = msg::load().expect("failed to decode input argument");
+    let ft: &mut FungibleToken = unsafe { FUNGIBLE_TOKEN.get_or_insert(Default::default()) };
+    debug!("{:?}", query);
+    let encoded = match query {
+        State::Name => StateReply::Name(ft.name.clone()),
+        State::TotalSupply => StateReply::TotalSupply(ft.total_supply),
+        State::BalanceOf(account) => {
+            let balance = ft.balances.get(&account).unwrap_or(&0);
+            StateReply::Balance(*balance)
         }
-        ContractAction::Owner { token_id } => {
-            msg::reply(
-                ContractEvent::Owner {
-                    owner: NFTCore::owner_of(contrato, token_id),
-                    token_id,
-                },
-                0,
-            )
-            .expect("Error during replying with `NFTEvent::Owner`");
-        }
-        ContractAction::Clear { transaction_hash } => contrato.clear(transaction_hash),
-    };
+    }
+    .encode();
+    gstd::util::to_leak_ptr(encoded)
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum State {
+    Name,
+    TotalSupply,
+    BalanceOf(ActorId),
+}
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+#[codec(crate = gstd::codec)]
+#[scale_info(crate = gstd::scale_info)]
+pub enum StateReply {
+    Name(String),
+    TotalSupply(u128),
+    Balance(u128),
 }
